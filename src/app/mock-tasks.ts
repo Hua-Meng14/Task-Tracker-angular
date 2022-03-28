a38effba428fec53baeda5416ca1e3bba155a8bc
Huameng Lim <lim.huameng19@kit.edu.kh>
2022-03-29 00:30:03 +0700

    Final Code - working - Task Tracker Angular with fake api - Json Server

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Learn Angular JS',
+    day: 'Mar 28 2022 at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Escott Internal Meeting',
+    day: 'Mar 29 2022 at 3:30pm',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'GCP week 8 1st meeting',
+    day: 'Mar 28 2022 at 4:30pm',
+    reminder: false,
+  },
+];
